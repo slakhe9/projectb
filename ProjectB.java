3422d9d9efbde1c4f90fb5885cab1307f2460df4
sarika lakhe <sarikalakhe1980@yahoo.co.uk>
2023-10-29 15:20:36 +0000

    projectb

---
+++ ProjectB.java
@@ -0,0 +1,20 @@
+package projectb;
+
+public class ProjectB {
+
+    public static void main(String[]args) {
+
+        int p =30;
+
+        if (p>=30){
+
+            System.out.println("entry is valid");
+        }else{
+            if(p<40){
+                System.out.println("invalid entry");
+            }
+        }
+    }
+
+    }
+
